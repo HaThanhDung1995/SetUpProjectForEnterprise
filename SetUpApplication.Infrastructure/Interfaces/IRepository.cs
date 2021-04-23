f2b9436c55ef69cc92149cf56028952e72e5e549
hathanhdung1995 <hathanhdung1995@gmail.com>
2021-04-23 10:24:13 +0700

    hello

---
+++ SetUpApplication.Infrastructure/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SetUpApplication.Infrastructure.Interfaces
+{
+    public interface IRepository<T, K> where T : DomainEntity<K>
+    {
+    }
+}
